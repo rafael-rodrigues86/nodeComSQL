a41deaf9468d1cf2aac5f4372b8305e41d91787f
Rafael Rodrigues <rafael.raraujo86@gmail.com>
2023-11-27 13:37:41 -0300

    Adicionar ON DELETE CASCADE nas FOREIGN KEY da table photo_tags

--- migrations/database.sql
+++ migrations/database.sql
@@ -53,7 +53,7 @@ CREATE TABLE tags (
 CREATE TABLE photo_tags (
     photo_id INTEGER NOT NULL,
     tag_id INTEGER NOT NULL,
-    FOREIGN KEY(photo_id) REFERENCES photos(id),
-    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id) ON DELETE CASCADE,
+    FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE,
     PRIMARY KEY(photo_id, tag_id)
 );
